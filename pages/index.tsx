674818d5183af67fa7d4e99735cc26d7786234cc
alquerqueee-eng <alquerqueee@gmail.com>
2026-08-13 05:51:30 -0500

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import HoyView from '../components/HoyView';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <HoyView />
+    </main>
+  );
+}
